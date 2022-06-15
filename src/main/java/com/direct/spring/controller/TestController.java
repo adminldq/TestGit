f3f3fa56442ad3be7113e3227de306ae9df37a08
admin <admin@direct.com>
2022-06-15 14:09:13 +0800

    first commit

---
+++ src/main/java/com/direct/spring/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.direct.spring.controller;
+
+
+import java.sql.SQLOutput;
+
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("git与idea整合。。。。。。。。。");
+    }
+}
