86a6ecb8fcd4bc8168c44324b1a17a83d1887572
admin <admin@direct.com>
2022-06-15 14:30:51 +0800

    second commit

--- src/main/java/com/direct/spring/controller/TestController.java
+++ src/main/java/com/direct/spring/controller/TestController.java
@@ -6,5 +6,6 @@ import java.sql.SQLOutput;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("git与idea整合。。。。。。。。。");
+        System.out.println("git1与idea整合。。。。。。。。。");
     }
 }
